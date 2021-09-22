c287258012d46f1f7d6551f9c3c700dbce13e95c
Tuan Khang <you@example.com>
2021-09-22 10:23:22 +0700

    main entity

---
+++ src/main/java/sv/iuh/spring_boot_jwt_ltk/SpringBootJwtLtkApplication.java
@@ -0,0 +1,13 @@
+package sv.iuh.spring_boot_jwt_ltk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtLtkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtLtkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sv/iuh/spring_boot_jwt_ltk/SpringBootJwtLtkApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.iuh.spring_boot_jwt_ltk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtLtkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
